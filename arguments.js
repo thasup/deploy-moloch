e76da6f4578006d6781d5370f93363553542a769
thasup <thanachonfirst@hotmail.com>
2022-09-30 17:05:00 +0700

    Add comment

--- arguments.js
+++ arguments.js
@@ -10,11 +10,11 @@ module.exports = [
   // uint256 _gracePeriodLength
   35,
   // uint256 _proposalDeposit
-  "1000000000000000000",
+  "1000000000000000000", // 1 ETH
   // uint256 _dilutionBound
   3,
   // uint256 _processingReward
-  "10000000000"
+  "10000000000" // 0.00000001 ETH
 ];
 
 // Use this command to verify the contract
